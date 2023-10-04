983d7602d624c30326b5424ff9f22c1b89ba98b9
Shan Wenxiao <seanoj_noreply@yeah.net>
2023-10-04 10:05:11 +0800

    Update XMOJ.user.js
    Signed-off-by: Shan Wenxiao <seanoj_noreply@yeah.net>

--- XMOJ.user.js
+++ XMOJ.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         XMOJ
-// @version      1.0.203
+// @version      1.0.207
 // @description  XMOJ增强脚本
 // @author       @langningchen, @PythonSmall-Q and @boomzero
 // @namespace    https://github/langningchen
