a25c21c933f052fc0631966f5bf0dff5ad80adc5
Zhu Chenrui <85378277+boomzero@users.noreply.github.com>
2023-10-09 21:44:47 +0800

    Update XMOJ.user.js
    Signed-off-by: Zhu Chenrui <85378277+boomzero@users.noreply.github.com>

--- XMOJ.user.js
+++ XMOJ.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         XMOJ
-// @version      1.0.219
+// @version      1.0.220
 // @description  XMOJ增强脚本
 // @author       @XMOJ-Script-dev, @langningchen and the community
 // @namespace    https://github/langningchen
